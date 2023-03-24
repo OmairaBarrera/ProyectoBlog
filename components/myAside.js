5140f8d5b3368cfcb53e4e88a7a1761b3fb5db00
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-23 22:29:32 -0500

    feat: :sparkles: Add info module of aside
    add info and links.

--- components/myAside.js
+++ components/myAside.js
@@ -1,48 +1,56 @@
 export default{
     nav:[
         {
-            title: "About",
-            parragrah: "Customize this section to tell your visitors a little bit about your publication, writers, content, or something else entirely. Totally up to you.",
+            title: "Una Mirada Femenina",
+            parragrah: "Las mujeres deben luchar por desigualdades sistematicas basadas en el genero, y en gran medida siguen siendo invisibles. Esto crea la necesidad de que se las escuche y se comuniquen sus experiencias y prespectivas unicas.",
         },
         {
-            title: "About",
-            parragrah: "Customize this section to tell your visitors a little bit about your publication, writers, content, or something else entirely. Totally up to you.",
-        },
-        {
-            title: "About",
+            title: "LIBROS",
             link: [
                 {
-                    name:"desencadenamiento",
-                    href: ""
+                    name:"Mujeres de ciencia",
+                    href: "https://www.buscalibre.com.co/libro-mujeres-de-ciencia/9788491873334/p/51434165"
                 },
                 {
-                    name:"desencadenamiento",
-                    href: ""
+                    name:"Mujeres olvidadas : las científicas",
+                    href: "https://www.buscalibre.com.co/libro-mujeres-olvidadas-las-cientificas-libros-singulares/9788441542143/p/52358812"
                 },
                 {
-                    name:"desencadenamiento",
-                    href: ""
+                    name:"Las Mentiras Científicas Sobre las Mujeres",
+                    href: "https://www.buscalibre.com.co/libro-las-mentiras-cientificas-sobre-las-mujeres/9788490972656/p/48005610"
+                },
+                {
+                    name:"Tierra de Mujeres",
+                    href: "https://www.buscalibre.com.co/libro-tierra-de-mujeres/9789569949890/p/53321725"
+                },
+                {
+                    name:"Visionarias. Inventoras Desconocidas",
+                    href: "https://www.buscalibre.com.co/libro-visionarias-inventoras-desconocidas/9788416670888/p/52360402"
+                },
+                {
+                    name:"Las chicas son de ciencias",
+                    href: "https://www.buscalibre.com.co/libro-las-chicas-son-de-ciencias/9788490438824/p/49582303"
                 }
             ],
         },
         {
-            title: "About",
-            parragrah: "Customize this section to tell your visitors a little bit about your publication, writers, content, or something else entirely. Totally up to you.",
-        },
-        {
-            title: "About",
+            title: "DOCUMENTALES",
             link: [
                 {
-                    name:"desencadenamiento",
-                    href: ""
+                    name:"Efecto Matilda",
+                    href: "https://youtu.be/Fx0ztzf-2V0"
                 },
                 {
-                    name:"desencadenamiento",
-                    href: ""
+                    name:"Mi mensaje a las niñas",
+                    href: "https://www.youtube.com/watch?v=HnGKvC4iDG0&ab_channel=AprendemosJuntos2030"
                 },
                 {
-                    name:"desencadenamiento",
-                    href: ""
+                    name:"La brecha de genero",
+                    href: "https://www.youtube.com/watch?v=i7juz-U5GAk&ab_channel=FECYTciencia"
+                },
+                {
+                    name:"Genero en las STEM",
+                    href: "https://www.youtube.com/watch?v=2G80ak9bZMw&ab_channel=TEDxTalks"
                 }
             ],
         },
